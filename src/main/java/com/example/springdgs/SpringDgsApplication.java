dbd465d2cba1a38955954afa7a661693fa501ada
Frossstyy85 <morgan.lindholm@gmail.com>
2025-06-11 22:03:52 +0200

    initial commit

---
+++ src/main/java/com/example/springdgs/SpringDgsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDgsApplication.class, args);
+	}
+
+}
